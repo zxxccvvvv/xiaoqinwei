252de00804ec5f36ee4baec25e1e07ef73fa8bb0
XQW_123 <70804059+zxxccvvvv@users.noreply.github.com>
2020-12-31 16:56:38 +0800

    Update CartController.java

--- gmall-cart/src/main/java/com/atguigu/gmall/cart/controller/CartController.java
+++ gmall-cart/src/main/java/com/atguigu/gmall/cart/controller/CartController.java
@@ -26,7 +26,7 @@ public class CartController {
         System.out.println("dev分支");
         System.out.println("dev1分支");
         System.out.println("dev2分支");
-        System.out.println("dev3分支");
+        System.out.println("de分支");
 
         return ResponseVo.ok(cartList);
 
